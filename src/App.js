8ede800300d434e7270727eea1b43c5a398370d5
Vinicius Oliver <viniciusoliver805@gmail.com>
2023-08-10 21:16:11 -0400

    Aula 10/08 json placeHolder, photosAndPost

--- src/App.js
+++ src/App.js
@@ -2,48 +2,76 @@ import './App.css';
 import { Component} from 'react';
 
 class App extends Component {
+
+
     state = {
-    posts: [
-      {
-        id: 1,
-        title: 'Título 1',
-        body: 'Corpo 1'
-      },
-      {
-        id: 1,
-        title: 'Título 2',
-        body: 'Corpo 2'
-      },
-      {
-        id: 1,
-        title: 'Título 3',
-        body: 'Corpo 3'
-      },
-      {
-        id: 1,
-        title: 'Título 4',
-        body: 'Corpo 4'
-      }
-    ]
-  } 
+      posts: [
+        {
+          id: 1,
+          title: 'Título 1',
+          body: 'Corpo 1'
+        },
+        {
+          id: 2,
+          title: 'Título 2',
+          body: 'Corpo 2'
+        },
+        {
+          id: 3,
+          title: 'Título 3',
+          body: 'Corpo 3'
+        },
+        {
+          id: 4,
+          title: 'Título 4',
+          body: 'Corpo 4'
+        },
+      ],
+  }; 
 
+componentDidMount(){      //funções ciclo de vida
+ this.loadPosts()
+}
 
+loadPosts = async () => {
+  const postsResponse = fetch("https://jsonplaceholder.typicode.com/posts")
+  const photosResponse = fetch("https://jsonplaceholder.typicode.com/photos")
+
+  const[posts, photos] = await Promise.all([postsResponse, photosResponse]) //captura dos dados
+  
+  const postsJson = await posts.json() // conversao dos dados
+  const photosJson = await photos.json() // conversao dos dados
+
+  const photosAndPosts = postsJson.map((post, index) => { //criamos um array
+    return {
+      ...post,
+      cover: photosJson[index].url,
+    }
+  })
+
+  this.setState({ posts: photosAndPosts }) //atualizar o estado da aplicação 
+}
 
 render () {
 
 const {posts} = this.state  //isso aqui é chamado de destructor 
 
   return (
-    <div className="App">
-      {posts.map((post) => ( //toda vez tem a arrowFunction afee
-      <div key={post.id}>
-        <h1>{post.title}</h1>
-        <h3>{post.body}</h3>
+    <section className='container'>
+      <div className="posts">
+        {posts.map((post) => ( //toda vez tem a arrowFunction afee
+        <div key={post.id} className='post'>
+          <img src={post.cover} alt="" />
+          <div className='text'>
+            <h1>{post.title}</h1>
+            <h3>{post.body}</h3>
+          </div>         
+         
+        </div>
+
+        ))}
       </div>
-
-      ))}
-    </div>
-
+    </section>
     
   );
 }
